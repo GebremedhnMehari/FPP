0b5c996bd48362954141a21b6a9669c749fd33a2
Gebremedhn Mehari <geb.meh@gmail.com>
2016-02-24 01:12:52 -0600

    edited 2 24 2016

--- src/demo/Labdemo.java
+++ src/demo/Labdemo.java
@@ -1,5 +1,7 @@
 package demo;
 
+import assignment2.Capitalize;
+
 public class Labdemo {
 	public static void main(String args[]) {
 		
@@ -14,5 +16,39 @@ public class Labdemo {
 			}
 			//System.out.println(result);
 			}*/
+		
+		//2. Character to lower 
+			/*char c='n';
+			System.out.print(toLower(c));
+		}
+     	public static char toLower(char ch){
+		if (ch>='A' && ch<='Z')
+		{
+			return (char) ((ch -'A')+'a');
+			
+		}
+		return ch;
+		}*/
+		
+		//3 Array Example
+		/*int [] arr = {4, 5, 12, 25};
+		for(int x: arr) {
+			System.out.print(x+" ");
+		}
+		String t = "Hello,strings can be fun.They have many uses.";
+				String[] result = t.split(",|\\.| ") ;
+				for(String x1: result) {
+					System.out.print(x1+" ");
+				}*/
+		// 4 Command line ????
+		/*
+		//int len = 0;
+				//if(args != null) len = args.length;
+				for(int i = 0; i < args.length; ++i) {  
+					System.out.println("position " + i + ": " + args[i]);
+		}
+				*/
+	} 
 	}
-}
+
+
