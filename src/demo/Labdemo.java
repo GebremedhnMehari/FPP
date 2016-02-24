053ea708b9daf4fdece73ef58c66d043c7940410
Gebremedhn Mehari <geb.meh@gmail.com>
2016-02-24 00:26:20 -0600

    withdemo

---
+++ src/demo/Labdemo.java
@@ -0,0 +1,18 @@
+package demo;
+
+public class Labdemo {
+	public static void main(String args[]) {
+		
+		//1. String Reverse
+				/*String str = "desserts";
+			//String result ="";
+			
+			for (int i = str.length()-1; i>=0; i--) {
+				//result = result+str.charAt(i);
+				//System.out.println(result);
+				System.out.print(str.charAt(i));
+			}
+			//System.out.println(result);
+			}*/
+	}
+}
